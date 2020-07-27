479398282de20797e0fa88de25594139ac6a6adb
zjx0905 <954270063@qq.com>
2020-07-27 15:34:15 +0800

    clear

--- src/utils/isEmptyObject.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 是一个空的对象吗?
- *
- * @param obj 要检查的对象
- */
-export default function isEmptyObject(obj: any): boolean {
-  return obj !== null && typeof obj === 'object' && Object.keys(obj).length === 0;
-}
